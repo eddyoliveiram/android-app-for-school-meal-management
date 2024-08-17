b3848a2b5eb8310612dbc9630252ef02d16259e7
Eddy Oliveira <eddy_eom@hotmail.com>
2024-08-16 23:35:13 -0300

    Changing project structure by adding backend-api

---
+++ backend-api/routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/posts', [PostController::class, 'index']);
+
+Route::middleware(['auth:sanctum','admin'])->group(function () {
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::put('/posts/{id}', [PostController::class, 'update']);
+    Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::get('/posts/{id}', [PostController::class, 'show']);
+});
